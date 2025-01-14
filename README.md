2b26b1157529de585b0d06e96698b5b5977bea9c
Joseph Hopson <jh@josephhopson.com>
2025-01-13 22:30:31 -0700

    style & font updates, added setting links

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-![Cover](assets/cover.webp)
+<!-- ![Cover](assets/cover.webp) -->
 
 # Fellowship 2E Module (Unofficial)
 > [!Warning]
 > This module is in the early stages of development, use with caution.
 
 <p align="center">
-    <img alt="Foundry Version 12 support" src="https://img.shields.io/badge/Foundry-v12-informational"> 
+    <img alt="Foundry Version 12 support" src="https://img.shields.io/badge/Foundry-v12-informational">
+    <img alt="Latest Release Download Count" src="https://img.shields.io/github/downloads/philote/fellowship-pbta/latest/total"> 
     <img alt="GitHub release (latest by date)" src="https://img.shields.io/github/v/release/philote/fellowship-pbta"> 
-    <img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/philote/fellowship-pbta"> 
-    <!-- <img alt="Latest Release Download Count" src="https://img.shields.io/github/downloads/philote/fellowship-pbta/latest/module.zip"> -->
-    <img alt="GitHub Release Date" src="https://img.shields.io/github/release-date/philote/fellowship-pbta?label=latest%20release" /> 
+    <img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/philote/fellowship-pbta">
+    <img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/philote/fellowship-pbta">
 </p>
 <p align="center">
     <img alt="GitHub" src="https://img.shields.io/github/license/philote/fellowship-pbta"> 
@@ -21,37 +21,53 @@
     </a> 
     <a href="https://github.com/philote/fellowship-pbta/stargazers">
         <img alt="GitHub stars" src="https://img.shields.io/github/stars/philote/fellowship-pbta">
-    </a> 
+    </a>
+</p>
+<p align="center">
+    <a href="https://ko-fi.com/G2G3I91JQ">
+        <img alt="ko-fi" src="https://ko-fi.com/img/githubbutton_sm.svg">
+    </a>
 </p>
 
 ### An unofficial Fellowship 2E module for Foundry VTT using the PbtA System
 Fellowship 2nd Edition is a tabletop role-playing game that emphasizes collaborative storytelling and world-building, inspired by epic journeys like The Lord of the Rings or Avatar: The Last Airbender. Players take on the roles of iconic heroes from distinct cultures, each wielding unique powers and abilities, as they band together to confront a shared adversary, known as the Overlord.
 
-The ideas used in this module are from Fellowship 2E from [Liberi Gothica Games](https://liberigothica.itch.io/). You can get the Books at: https://liberigothica.itch.io/fellowship-a-tabletop-adventure-game
+The ideas used in this module are from Fellowship 2E from [Liberi Gothica Games](https://liberigothica.itch.io/) Written by [Vel Mini](https://bsky.app/profile/velimini.bsky.social). You can get the Books at [itch.io](https://liberigothica.itch.io/fellowship-a-tabletop-adventure-game)
 
 This module is for [Foundry VTT](https://foundryvtt.com/) and requires the [PbtA System](https://github.com/asacolips-projects/pbta) created by [asacolips](https://github.com/asacolips).
 
 ## Screenshot
-![Screenshot](assets/screenshot.webp)
+![Screenshot of the character sheet](assets/screenshot.webp)
+![Screenshot of some of the items in the compendium](assets/screenshot2.webp)
 
 ## Dependencies
 The following Foundry VTT game system must be installed to use this module: [Powered by the Apocalypse](https://foundryvtt.com/packages/pbta).
 
 ## How to Install
-You can install the latest released version of the module by using this manifest link in Foundry VTT. [Instructions](https://foundryvtt.com/article/tutorial/): https://github.com/philote/fellowship-pbta/releases/latest/download/system.json
+You can install the latest released version of the module by using this manifest link in Foundry VTT. [Instructions](https://foundryvtt.com/article/tutorial/): [module.json](https://github.com/philote/fellowship-pbta/releases/latest/download/module.json)
 
 ## Features
-- Character sheet
-    - PbtA Playbook support
+- Character Sheet
+    - PbtA Character Playbook support
+    - Added Destiny support
+    - Added Companion support
+    - sheet designs
+- NPC Sheet
+- Overload Sheet
+- Item Compendiums
+    - Book 1 Playbooks
+    - Book 1 Destinies
+    - Playbook Companions
+    - Playbook Gear
+    - All the Book 1 Moves
 
 ## TODO
 - Overlord sheet
-- NPC sheet
-- Create support for companions
-- Create support for Destiny Playbooks
-- Styling
+- NPC sheet cleanup
+- Finish adding gear and companions
 
 # License & Acknowledgements
-[Fellowship 2nd Edition](https://liberigothica.itch.io/fellowship-a-tabletop-adventure-game) was created by [Vel Mini](@velimini.bsky.social) and is copyright (2015-2019) by Vel Mini and [Liberi Gothica Games](https://liberigothica.itch.io/).
+[Fellowship 2nd Edition](https://liberigothica.itch.io/fellowship-a-tabletop-adventure-game) was created by [Vel Mini](@velimini.bsky.social) and is copyright (2015-2019) by Vel Mini and [Liberi Gothica Games](https://liberigothica.itch.io/). The content in the book Fellowship 2e book is under the Creative Commons CC-BY-SA License:
+https://creativecommons.org/licenses/by-sa/4.0/
 
-Icons from game-icons.net are released under a Creative Commons Attribution 3.0 Unported license. https://creativecommons.org/licenses/by/3.0/
+<!-- Icons from game-icons.net are released under a Creative Commons Attribution 3.0 Unported license. https://creativecommons.org/licenses/by/3.0/ -->
--- module/fellowship-pbta.mjs
+++ module/fellowship-pbta.mjs
@@ -1,177 +1,247 @@
-import * as pbtaConfig from './helpers/pbta-config.mjs';
+import * as pbtaConfig from "./helpers/pbta-config.mjs";
 import * as utils from "./helpers/utils.mjs";
 import { FELLOWSHIP } from "./helpers/config.mjs";
-import { CompanionModel } from './data/companionModel.mjs';
-import { CompanionSheet } from './sheets/companionSheet.mjs';
-import { DestinyModel } from './data/destinyModel.mjs';
-import { DestinySheet } from './sheets/destinySheet.mjs';
-import { FellowshipActorSheetMixin } from './sheets/actor-sheet.mjs';
-import { CharacterModel } from './data/characterModel.mjs';
+import { CompanionModel } from "./data/companionModel.mjs";
+import { CompanionSheet } from "./sheets/companionSheet.mjs";
+import { DestinyModel } from "./data/destinyModel.mjs";
+import { DestinySheet } from "./sheets/destinySheet.mjs";
+import { FellowshipActorSheetMixin } from "./sheets/actor-sheet.mjs";
+import { CharacterModel } from "./data/characterModel.mjs";
 
-Hooks.once('init', () => {
-    // Fellowship ActorSheet Setup
-    CONFIG.Actor.dataModels.character = CharacterModel;
-    const fellowshipActorSheet = FellowshipActorSheetMixin(game.pbta.applications.actor.PbtaActorSheet);
-    Actors.unregisterSheet('pbta', game.pbta.applications.actor.PbtaActorSheet, { types: ['character'] });
-    Actors.registerSheet('pbta', fellowshipActorSheet, {
-        types: ['character'],
-        makeDefault: true,
-        label: 'FELLOWSHIP.SheetConfig.character',
-    });
+Hooks.once("init", () => {
+	// Fellowship ActorSheet Setup
+	CONFIG.Actor.dataModels.character = CharacterModel;
+	const fellowshipActorSheet = FellowshipActorSheetMixin(
+		game.pbta.applications.actor.PbtaActorSheet
+	);
+	Actors.unregisterSheet("pbta", game.pbta.applications.actor.PbtaActorSheet, {
+		types: ["character"],
+	});
+	Actors.registerSheet("pbta", fellowshipActorSheet, {
+		types: ["character"],
+		makeDefault: true,
+		label: "FELLOWSHIP.SheetConfig.character",
+	});
 
-    // Companion DataModel & Sheet Setup
-    Object.assign(CONFIG.Item.dataModels, {
-        'fellowship-pbta.companion': CompanionModel,
-    });
-    Items.unregisterSheet('pbta', game.pbta.applications.item.PbtaItemSheet, { types: ['fellowship-pbta.companion'] });
-    Items.registerSheet('fellowship-pbta', CompanionSheet, {
-        types: ['fellowship-pbta.companion'],
-        makeDefault: true,
-        label: 'Companion Sheet',
-    });
+	// Companion DataModel & Sheet Setup
+	Object.assign(CONFIG.Item.dataModels, {
+		"fellowship-pbta.companion": CompanionModel,
+	});
+	Items.unregisterSheet("pbta", game.pbta.applications.item.PbtaItemSheet, {
+		types: ["fellowship-pbta.companion"],
+	});
+	Items.registerSheet("fellowship-pbta", CompanionSheet, {
+		types: ["fellowship-pbta.companion"],
+		makeDefault: true,
+		label: "Companion Sheet",
+	});
 
-    // Destiny DataModel & Sheet Setup
-    Object.assign(CONFIG.Item.dataModels, {
-        'fellowship-pbta.destiny': DestinyModel,
-    });
-    Items.unregisterSheet('pbta', game.pbta.applications.item.PbtaItemSheet, { types: ['fellowship-pbta.destiny'] });
-    Items.registerSheet('fellowship-pbta', DestinySheet, {
-        types: ['fellowship-pbta.destiny'],
-        makeDefault: true,
-        label: 'Destiny Sheet',
-    });
+	// Destiny DataModel & Sheet Setup
+	Object.assign(CONFIG.Item.dataModels, {
+		"fellowship-pbta.destiny": DestinyModel,
+	});
+	Items.unregisterSheet("pbta", game.pbta.applications.item.PbtaItemSheet, {
+		types: ["fellowship-pbta.destiny"],
+	});
+	Items.registerSheet("fellowship-pbta", DestinySheet, {
+		types: ["fellowship-pbta.destiny"],
+		makeDefault: true,
+		label: "Destiny Sheet",
+	});
 
-    // Register settings
-    game.settings.register('fellowship-pbta', 'firstTime', {
-        name: game.i18n.localize('FELLOWSHIP.Settings.startup.name'),
-        scope: 'world',
-        config: false,
-        type: Boolean,
-        default: true
-    });
+	// Register settings
+	game.settings.register("fellowship-pbta", "firstTime", {
+		name: game.i18n.localize("FELLOWSHIP.Settings.startup.name"),
+		scope: "world",
+		config: false,
+		type: Boolean,
+		default: true,
+	});
 
-    game.settings.register('fellowship-pbta', 'enableLoginImg', {
-        name: game.i18n.localize('FELLOWSHIP.Settings.img.name'),
-        hint: game.i18n.localize('FELLOWSHIP.Settings.img.hint'),
-        scope: 'world',
-        config: true,
-        type: Boolean,
-        default: false
-      });
+	game.settings.register("fellowship-pbta", "enableLoginImg", {
+		name: game.i18n.localize("FELLOWSHIP.Settings.img.name"),
+		hint: game.i18n.localize("FELLOWSHIP.Settings.img.hint"),
+		scope: "world",
+		config: true,
+		type: Boolean,
+		default: false,
+	});
 
-    // Preload Handlebars stuff.
-    utils.preloadHandlebarsTemplates();
+	// Preload Handlebars stuff.
+	utils.preloadHandlebarsTemplates();
 
-    CONFIG.FELLOWSHIP = FELLOWSHIP;
+	CONFIG.FELLOWSHIP = FELLOWSHIP;
 });
 
 // PbtA configuration hook
-Hooks.once('pbtaSheetConfig', () => {
-    // https://github.com/asacolips-projects/pbta?tab=readme-ov-file#overriding-sheet-config-in-a-module
-    // Disable the PbtA sheet config form.
-    if (game.settings.settings.get('pbta.sheetConfigOverride')) game.settings.set('pbta', 'sheetConfigOverride', true);
+Hooks.once("pbtaSheetConfig", () => {
+	// https://github.com/asacolips-projects/pbta?tab=readme-ov-file#overriding-sheet-config-in-a-module
+	// Disable the PbtA sheet config form.
+	if (game.settings.settings.get("pbta.sheetConfigOverride"))
+		game.settings.set("pbta", "sheetConfigOverride", true);
 
-    // Replace the game.pbta.sheetConfig with this module's version.
-    pbtaConfig.configSheet();
+	// Replace the game.pbta.sheetConfig with this module's version.
+	pbtaConfig.configSheet();
 
-    // PBTA Settings
-    pbtaConfig.pbtaSettings();
+	// PBTA Settings
+	pbtaConfig.pbtaSettings();
 
-    // Defines custom PbtA system tags.
-    game.pbta.tagConfigOverride = pbtaConfig.tagConfig;
+	// Defines custom PbtA system tags.
+	game.pbta.tagConfigOverride = pbtaConfig.tagConfig;
 });
 
-Hooks.once('ready', async function () {
-    // Get destinies in world and compendium, with code so Babele works
-    if (!(game.modules.get("babele")?.active && game.i18n.lang !== "en")) {
+Hooks.once("ready", async function () {
+	// Get destinies in world and compendium, with code so Babele works
+	if (!(game.modules.get("babele")?.active && game.i18n.lang !== "en")) {
 		utils.getDestinies();
 	}
-    
-    // This allows the module to set a custom image to the login screen
-    if (!game.user.isGM) return;
-    if (game.settings.get('fellowship-pbta', 'firstTime')) {
-        game.settings.set('fellowship-pbta', 'firstTime', false);
-    
-        const callback = async () => {
-            game.settings.set('fellowship-pbta', 'enableLoginImg', true);
-            const worldData = {
-                id: game.world.id,
-                action: 'editWorld',
-                background: `modules/fellowship-pbta/assets/cover.webp`,
-            };
-            let response;
-            try {
-                response = await foundry.utils.fetchJsonWithTimeout(foundry.utils.getRoute('setup'), {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify(worldData),
-            });
-            if (response.error) {
-                ui.notifications.error(response.error);
-            } else if (!response) {
-                game.world.updateSource(response);
-            }
-            } catch (e) {
-                return ui.notifications.error(e);
-            }
-        };
-  
-        foundry.applications.api.DialogV2.confirm({
-            window: { title: game.i18n.localize('FELLOWSHIP.Settings.startup.dialog.title') },
-            content: game.i18n.localize('FELLOWSHIP.Settings.startup.dialog.content'),
-            rejectClose: false,
-            modal: true,
-            yes: { callback: callback },
-        });
-    } else {
-        if (game.settings.get('fellowship-pbta', 'enableLoginImg')) {
-            const worldData = {
-                id: game.world.id,
-                action: 'editWorld',
-                background: `modules/fellowship-pbta/assets/cover.webp`,
-            };
-            let response;
-            try {
-                response = await foundry.utils.fetchJsonWithTimeout(foundry.utils.getRoute('setup'), {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify(worldData),
-            });
-            if (response.error) {
-                ui.notifications.error(response.error);
-            } else if (!response) {
-                game.world.updateSource(response);
-            }
-            } catch (e) {
-                return ui.notifications.error(e);
-            }
-        }
-    }
+
+	// This allows the module to set a custom image to the login screen
+	if (!game.user.isGM) return;
+	if (game.settings.get("fellowship-pbta", "firstTime")) {
+		game.settings.set("fellowship-pbta", "firstTime", false);
+
+		const callback = async () => {
+			game.settings.set("fellowship-pbta", "enableLoginImg", true);
+			const worldData = {
+				id: game.world.id,
+				action: "editWorld",
+				background: `modules/fellowship-pbta/assets/cover.webp`,
+			};
+			let response;
+			try {
+				response = await foundry.utils.fetchJsonWithTimeout(
+					foundry.utils.getRoute("setup"),
+					{
+						method: "POST",
+						headers: { "Content-Type": "application/json" },
+						body: JSON.stringify(worldData),
+					}
+				);
+				if (response.error) {
+					ui.notifications.error(response.error);
+				} else if (!response) {
+					game.world.updateSource(response);
+				}
+			} catch (e) {
+				return ui.notifications.error(e);
+			}
+		};
+
+		foundry.applications.api.DialogV2.confirm({
+			window: {
+				title: game.i18n.localize("FELLOWSHIP.Settings.startup.dialog.title"),
+			},
+			content: game.i18n.localize("FELLOWSHIP.Settings.startup.dialog.content"),
+			rejectClose: false,
+			modal: true,
+			yes: { callback: callback },
+		});
+	} else {
+		if (game.settings.get("fellowship-pbta", "enableLoginImg")) {
+			const worldData = {
+				id: game.world.id,
+				action: "editWorld",
+				background: `modules/fellowship-pbta/assets/cover.webp`,
+			};
+			let response;
+			try {
+				response = await foundry.utils.fetchJsonWithTimeout(
+					foundry.utils.getRoute("setup"),
+					{
+						method: "POST",
+						headers: { "Content-Type": "application/json" },
+						body: JSON.stringify(worldData),
+					}
+				);
+				if (response.error) {
+					ui.notifications.error(response.error);
+				} else if (!response) {
+					game.world.updateSource(response);
+				}
+			} catch (e) {
+				return ui.notifications.error(e);
+			}
+		}
+	}
 });
 
 Hooks.on("renderActorSheet", async (app, html) => {
-    // Character sheet customization
-    if (app.actor.type === "character") {
-        // Hide and show each stat based on playbook attributes
-        const playbook = app.actor.items.find((i) => i.type === "playbook");
-        if (playbook == undefined) {
-            // Initially hide iron and doom stats
-            const iron = html[0].querySelector('li.stat[data-stat="iron"]');
-            const doom = html[0].querySelector('li.stat[data-stat="doom"]');
-            if (iron) iron.classList.toggle("hidden", true);
-            if (doom) doom.classList.toggle("hidden", true);
-        } else {
-            Object.values(playbook.system.attributes.statsVisibility.choices[0].options).forEach(option => {
-                const statElement = html[0].querySelector(`li.stat[data-stat="${option.label}"]`);
-                if (statElement) {
-                    statElement.classList.toggle("hidden", !option.value);
-                }
-            });
-        }
-    }
+	// Character sheet customization
+	if (app.actor.type === "character") {
+		// Hide and show each stat based on playbook attributes
+		const playbook = app.actor.items.find((i) => i.type === "playbook");
+		if (playbook == undefined) {
+			// Initially hide iron and doom stats
+			const iron = html[0].querySelector('li.stat[data-stat="iron"]');
+			const doom = html[0].querySelector('li.stat[data-stat="doom"]');
+			if (iron) iron.classList.toggle("hidden", true);
+			if (doom) doom.classList.toggle("hidden", true);
+		} else {
+			Object.values(
+				playbook.system.attributes.statsVisibility.choices[0].options
+			).forEach((option) => {
+				const statElement = html[0].querySelector(
+					`li.stat[data-stat="${option.label}"]`
+				);
+				if (statElement) {
+					statElement.classList.toggle("hidden", !option.value);
+				}
+			});
+		}
+	}
 });
 
 Hooks.once("babele.ready", () => {
 	// It is a mystery why Babele calls "babele.ready" twice
 	Hooks.once("babele.ready", () => utils.getDestinies());
+});
+
+Hooks.on("renderSettings", (app, html) => {
+    const header = document.createElement("h2");
+    header.innerText = game.i18n.localize('FELLOWSHIP.Settings.game.heading');
+
+    const pbtaSettings = document.createElement("div");
+    html.find("#settings-game")?.after(header, pbtaSettings);
+
+    const buttons = [
+        {
+            action: (ev) => {
+                ev.preventDefault();
+                window.open("https://liberigothica.itch.io/", "_blank");
+            },
+            iconClasses: ["fa-solid", "fa-book"],
+            label: game.i18n.localize('FELLOWSHIP.Settings.game.publisher.title')
+        },
+        {
+            action: (ev) => {
+                ev.preventDefault();
+                window.open("https://github.com/philote/fellowship-pbta", "_blank");
+            },
+            iconClasses: ["fab", "fa-github"],
+            label: game.i18n.localize(`FELLOWSHIP.Settings.game.github.title`)
+        },
+        {
+            action: (ev) => {
+                ev.preventDefault();
+                window.open("https://ko-fi.com/ephson", "_blank");
+            },
+            iconClasses: ["fa-solid", "fa-mug-hot"],
+            label: game.i18n.localize("FELLOWSHIP.Settings.game.kofi.title")
+        },
+    ].map(({ action, iconClasses, label }) => {
+        const button = document.createElement("button");
+        button.type = "button";
+
+        const icon = document.createElement("i");
+        icon.classList.add(...iconClasses);
+
+        button.append(icon, game.i18n.localize(label));
+
+        button.addEventListener("click", action);
+
+        return button;
+    });
+
+    pbtaSettings.append(...buttons);
 });
